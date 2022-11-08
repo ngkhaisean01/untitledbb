ff6616c3b7ffd3b1dea94f1f9a4aa85c27e0e2f6
ngkhaisean01 <117711430+ngkhaisean01@users.noreply.github.com>
2022-11-08 14:34:41 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
 basic.forever(function () {
-	
+    if (input.lightLevel() < 0) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        for (let index = 0; index < 3; index++) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(200)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            basic.pause(200)
+        }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+    }
 })
